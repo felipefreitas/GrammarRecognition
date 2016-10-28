7d4051581af1f7dce758e22eadaeadcb00ac496f
Felipe Freitas <felipesafreitas@gmail.com>
2016-10-27 22:47:24 -0200

    init project and added file reader to process automata

---
+++ src/GrammarRecognition/GrammarRecognition/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GrammarRecognition
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Count() == 0)
+            {
+                Console.WriteLine($"Usar: verificador [GR de entrada]");
+                Environment.Exit(0);
+            }
+
+            #region FILE PRE PROCESSOR
+            string filePath = args[0];
+
+            using (var file = new StreamReader(filePath))
+            {
+                string content = "";
+                content = file.ReadToEnd();
+                content = Regex.Replace(content, @"\n|\r|\s+", "");
+
+            }
+            #endregion
+
+        }
+
+        public class Automata
+        {
+            public char[] Alphabet { get; set; }
+            public string InitialNode { get; set; }
+            public List<string> States { get; set; }
+            public List<Tuple<string, char, string>> Transitions { get; set; }
+            public List<string> FinalNodes { get; set; }
+
+            public Automata()
+            {
+                States = new List<string>();
+                Transitions = new List<Tuple<string, char, string>>();
+                FinalNodes = new List<string>();
+            }
+        }
+    }
+}
